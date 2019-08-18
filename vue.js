c5e310081fc47730b476eef4ecd9043fb71440e5
EkaterinaBelonosova <33663086+EkaterinaBelonosova@users.noreply.github.com>
2019-08-18 22:21:09 +0700

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,110 @@
+/*new Vue({
+        el: '#app',   
+         data: {
+            results:{
+            "BTC":{
+                "id": "535b16df-81a7-40ac-a3a2-58cd193a4dbd",
+                "type": "Full Time",
+                "url": "https://jobs.github.com/positions/535b16df-81a7-40ac-a3a2-58cd193a4dbd",
+                "created_at": "Fri Aug 16 12:08:56 UTC 2019",
+                "company": "Lake Shore Cryotronics, inc",
+                "company_url": null,
+                "location": "Westerville",
+                "title": ".NET Software Development Engineer (Desktop)",
+                "description": "\u003cp\u003e.NET Software Development Engineer (Desktop)\u003c/p\u003e\n\u003cp\u003eApply here: \u003ca href=\"https://lakeshore.clearcompany.com/careers/jobs/1215e1c6-9886-5727-c6fe-12a9a71eafd4/apply?source=1107672-CS-33968\"\u003ehttps://lakeshore.clearcompany.com/careers/jobs/1215e1c6-9886-5727-c6fe-12a9a71eafd4/apply?source=1107672-CS-33968\u003c/a\u003e\u003c/p\u003e\n\u003cp\u003eLake Shore Cryotronics is an international leader in developing innovative measurement and control solutions, primarily for scientific applications. Our equipment is often used to characterize physical properties of early stage electronic and magnetic materials.\u003c/p\u003e\n\u003cp\u003eOur Software Development Engineers are high-caliber, self-motivated professionals with a passion for designing, developing, and testing software in a fast-paced agile team environment. The ideal candidate will have a strong interest in using their skills to help advance the state of science that benefits mankind, as this is Lake Shore's mission.\u003c/p\u003e\n\u003cp\u003eSome Primary Duties Include:\u003c/p\u003e\n\u003cp\u003eDeveloping software primarily using C#/.NET in Visual Studio, and occasionally in other similar languages (VB.NET, Java, Python) in a Windows environment, targeting either desktop (Windows Forms / WPF) or mobile (Xamarin / Xamarin.Forms)\nWriting clean, readable, self-documenting code using proven software development principles such as SOLID\nWriting unit tests sufficient to balance high quality with high efficiency\nParticipating in a Scrum development process with a cross-functional team, providing feedback into the process for continuous improvement\nPerforming sustaining engineering, manufacturing, and service support for existing product lines as required\nDeveloping and maintaining Continuous Integration (CI) and Continuous Delivery (CD) pipelines using Azure DevOps, and other related technologies\nDeveloping and performing software verification and validation Quality Assurance (QA) testing\nUsing GIT code repository system, following prescribed branching strategies\nActively growing your design capability; taking ownership of your career providing regular self-evaluations and development plans\nKeeping up with the state of the art of software development by attending conferences, reading publications, and/or participating in relevant organizations\nQualified candidates will possess the following:\u003c/p\u003e\n\u003cp\u003eBachelor’s degree in Computer Science Engineering, or similar, with 0 – 5 years of relevant experience\nExperience with C#/.NET and Visual Studio\nExperience with unit testing frameworks and related technologies\nExperience with Xamarin / Xamarin.Forms technologies a plus\nExperience with Scrum or similar agile methodologies a plus\nExperience using GIT a plus\nExperience with CI / CD and other DevOps concepts, particularly using Azure DevOps a plus\nLake Shore Cryotronics is located in Westerville, a northeast suburb of Columbus OH and offers a work-life balancewith challenging assignments, a focus on health and wellness, social activities, and community outreach. We are privately held and celebrating our 50th year in business.\u003c/p\u003e\n\u003cp\u003eWe offer our employees a competitive compensation and benefits package (Medical, Dental, Vision, Life, and more). We are a recognized ColumbusCEO Top Workplace and a drug and tobacco free company.\u003c/p\u003e\n\u003cp\u003eLearn more about Lake Shore Cryotronics by visiting our website at \u003ca href=\"http://www.lakeshore.com\"\u003ewww.lakeshore.com\u003c/a\u003e – and also visit our Facebook and LinkedIn sites.\u003c/p\u003e\n\u003cp\u003eEqual Opportunity Employer of Minorities/Females/Disabled/Veterans\u003c/p\u003e\n\u003cp\u003eApply here: \u003ca href=\"https://lakeshore.clearcompany.com/careers/jobs/1215e1c6-9886-5727-c6fe-12a9a71eafd4/apply?source=1107672-CS-33968\"\u003ehttps://lakeshore.clearcompany.com/careers/jobs/1215e1c6-9886-5727-c6fe-12a9a71eafd4/apply?source=1107672-CS-33968\u003c/a\u003e\u003c/p\u003e\n",
+                "how_to_apply": "\u003cp\u003eSubmit an application by following this link:\u003c/p\u003e\n\u003cp\u003e\u003ca href=\"https://lakeshore.clearcompany.com/careers/jobs/1215e1c6-9886-5727-c6fe-12a9a71eafd4/apply?source=1107672-CS-33968\"\u003ehttps://lakeshore.clearcompany.com/careers/jobs/1215e1c6-9886-5727-c6fe-12a9a71eafd4/apply?source=1107672-CS-33968\u003c/a\u003e\u003c/p\u003e\n",
+                "company_logo": "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcnB2IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--80ab046034d8ad78a42b7cb7ab6dddc900c905a7/BlueBoxWithTagline.jpg"
+            },
+            "ETH":{
+                "id": "0187a87b-9bff-44fc-99bd-dbba2cec5db8",
+                "type": "Full Time",
+                "url": "https://jobs.github.com/positions/0187a87b-9bff-44fc-99bd-dbba2cec5db8",
+                "created_at": "Thu Aug 15 13:27:57 UTC 2019",
+                "company": "Daimler AG",
+                "company_url": "http://www.daimler.com",
+                "location": "Sindelfingen, Germany",
+                "title": "Developers! Code the future - Recruiting Day Automated Driving (w/m/d)",
+                "description": "\u003ch1\u003eTasks\u003c/h1\u003e\n\u003ch3\u003eAre you looking for new challenges as a #softwaredeveloper?\u003c/h3\u003e\n\u003cp\u003eAre you fascinated with state-of-the-art cars and their #assistance systems?\u003c/p\u003e\n\u003cp\u003e#Teamwork in the automotive premium segment is your thing?\u003c/p\u003e\n\u003cp\u003eThere are great things ahead of you: Together with you we would like to put the mobility of the future on the road - literally. What do we need to achieve that? YOU! Developers with future visions, fascination for data \u0026amp; passion for automobile. #nextbigthing\u003c/p\u003e\n\u003cp\u003eYou will code with programming languages such as C and C++ in a model-based way as well as design and conceptualize software architectures accordingly. Apply your knowledge in innovative technologies such as machine learning and big data and your creativity in an agile environment to drive forward the next generation technologies of automated driving to level 4. Together with international partners, we will work on virtual and real ECUs, communicate interdisciplinary and focus continuously on our customers.\u003c/p\u003e\n\u003ch3\u003eSo, are you in?\u003c/h3\u003e\n\u003cp\u003eApply directly here for our Recruiting Day Developers! Code the future on 19.10.2019.\u003c/p\u003e\n\u003ch3\u003eWhat do we have in store for you?\u003c/h3\u003e\n\u003cul\u003e\n\u003cli\u003e\n\u003cp\u003eInspiring Pitches about our vision of the future and the possibility to get to know the area of automated driving more in detail.\u003c/p\u003e\n\u003c/li\u003e\n\u003cli\u003e\n\u003cp\u003eExperience and discuss the state-of-the-art technology at our driving events directly with our experts.\u003c/p\u003e\n\u003c/li\u003e\n\u003cli\u003e\n\u003cp\u003eExchanges with your (potential) future colleagues about automated driving and AI in a relaxed atmosphere.\u003c/p\u003e\n\u003c/li\u003e\n\u003c/ul\u003e\n\u003ch3\u003eWe have a wide range of exciting topics in store for you! Which topic makes your heart beat faster? Please name 1-2 of your favorite topics in your application:\u003c/h3\u003e\n\u003cul\u003e\n\u003cli\u003e\n\u003cp\u003eSoftware development in the field of driving and parking functions, vision, fusion, plan, act and artificial intelligence\u003c/p\u003e\n\u003c/li\u003e\n\u003cli\u003e\n\u003cp\u003eSoftware development and architecture design for cutting edge multiprocessor/multicore embedded systems\u003c/p\u003e\n\u003c/li\u003e\n\u003cli\u003e\n\u003cp\u003eSystem design and software development for big data analytics\u003c/p\u003e\n\u003c/li\u003e\n\u003cli\u003e\n\u003cp\u003eDevOps Engineers for Software Tool Chain and Developer Experience\u003c/p\u003e\n\u003c/li\u003e\n\u003cli\u003e\n\u003cp\u003eSolution Architect for IT Infrastructure\u003c/p\u003e\n\u003c/li\u003e\n\u003cli\u003e\n\u003cp\u003eSoftware development in software simulation environment, sensor models\u003c/p\u003e\n\u003c/li\u003e\n\u003c/ul\u003e\n\u003ch1\u003eQualifications\u003c/h1\u003e\n\u003ch2\u003eWe are looking for you!\u003c/h2\u003e\n\u003ch3\u003ePerfect match for us:\u003c/h3\u003e\n\u003cp\u003eCompleted Studies in Computer Science, Electrical Engineering, Robotics, Mechatronics, Geodesy, Mathematics, Physics or a comparable subject area\ndistinct knowledge in programming languages (e.g. C, C++, Python), as well as in development and simulation tools (e.g. Matlab, Simulink, DOORS, Linux)\u003c/p\u003e\n\u003ch3\u003eUltra match for us:\u003c/h3\u003e\n\u003cp\u003eSound professional experience in the field of automotive software development or\nExperience in embedded software development\nPractical experience with real-time systems\u003c/p\u003e\n\u003ch3\u003eWhat are we offering:\u003c/h3\u003e\n\u003cp\u003eAn attractive salary with numerous additional benefits: flexible working time model, international assignments, individual development opportunities and a diverse range of health and sports activities. We focus on the individual! With us you work in a very future-oriented area - at the pulse of the time with modern technical equipment to work flexible and mobile from everywhere.\u003c/p\u003e\n\u003ch3\u003eGood to know:\u003c/h3\u003e\n\u003cp\u003eThe RecruitingDay \" Developers! Code the future \" takes place in Sindelfingen.\nThe application process consists of two steps:\u003c/p\u003e\n\u003cp\u003eStep 1: Online application\u003c/p\u003e\n\u003cp\u003eStep 2: After your succesful application you will receive a personal invitation approx. 2 weeks before the event\u003c/p\u003e\n",
+                "how_to_apply": "\u003cp\u003eJoin the team and apply now!\n\u003ca href=\"https://jobs.daimler.com/job/244214/developers-code-the-future-recruiting-day-automated-driving-w-m-d.html?lang=en\"\u003eclick here\u003c/a\u003e\u003c/p\u003e\n",
+                "company_logo": "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWh2IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f4bd7e595ce7044456cb32770a7dc796276b67e8/F-015-Luxury-in-Motion.jpg"
+            }
+        }
+    }       
+});*/
+/*new Vue({
+    el: '#res',
+    data() {
+      return {
+        info: null
+      };
+    },
+    mounted() {
+      axios
+      .get('https://jobs.github.com/positions.json')
+        .then(response => (this.info = response.data));
+    }
+  });*/
+  var pageNumber = 1;
+  var urlad = 'https://jobs.github.com/positions.json?page='+pageNumber+'&search=code';
+  var app = new Vue({
+    el: '#res',
+    data() {
+        return {
+            info: null,
+            loading: true,
+            errored: false
+          }
+
+    },
+    created () {
+        // загружаем данные, когда представление создано
+        // и данные реактивно отслеживаются
+        this.fetchData()
+      },
+    watch: {
+        // при изменениях маршрута запрашиваем данные снова
+        '$route': 'fetchData'
+      },
+   /* mounted(){
+        axios
+        .get(urlad) // загружаем данные
+        .then(response => (this.info = response.data)) // получаем  ответ
+        .catch(error => {
+            console.log(error);
+            this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+
+    },*/
+    methods: {
+        fetchData () {
+            axios
+            .get(urlad) // загружаем данные
+            .then(response => (this.info = response.data)) // получаем  ответ
+            .catch(error => {
+                console.log(error);
+                this.errored = true;
+            })
+            .finally(() => (this.loading = false));
+        },
+        nextPage: function(){
+            pageNumber++;
+            console.log(pageNumber);
+            console.log(urlad);
+            fetchData ();
+         },
+         prevPage(){
+           pageNumber--;
+           console.log(this.pageNumber);
+         }
+      },
+    filters: {
+        setDate (value) {
+          return new Date(value).toLocaleDateString();
+        }
+    },
+    
+  });
+
+
+   
