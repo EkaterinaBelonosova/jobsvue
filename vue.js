3e959ea8d0e3ab83da93826a87eb06998d7bb981
EkaterinaBelonosova <33663086+EkaterinaBelonosova@users.noreply.github.com>
2019-08-20 17:46:21 +0700

    3

--- vue.js
+++ vue.js
@@ -1,16 +1,29 @@
+function  urlad(page, desc) {
+  return 'https://jobs.github.com/positions.json?page='+ page + desc;
+}
 
-  function  urlad(page) {
-    return 'https://jobs.github.com/positions.json?page='+page;
-  }
+Vue.component('preview', {
+    template: '#view',
+    props: 'name',
+    data: function() {
+        return {
+          	name:name,
+        	}
+    },
+});
+
   var app = new Vue({
-    el: '#res',
+    el: '#jobs',
+    router: router,
     data() {
         return {
             nextResults: null,
             results: null,          
             loading: true,
             errored: false,
-            pageNumber: 1
+            pageNumber: 1,
+            description: null,
+            checked: null
           }
 
     },
@@ -23,31 +36,18 @@
     watch: {
         // при изменениях маршрута запрашиваем данные снова
         '$route': 'nextData',
-        '$route': 'fetchData'
-      
-      },
-      
-   /* mounted(){
-        axios
-        .get(urlad) // загружаем данные
-        .then(response => (this.info = response.data)) // получаем  ответ
-        .catch(error => {
-            console.log(error);
-            this.errored = true;
-        })
-        .finally(() => (this.loading = false));
-
-    },*/
+        '$route': 'fetchData'     
+    },
     methods: {
         nextData: function () {
             axios
-            .get(urlad(this.pageNumber+1)) // загружаем данные
+            .get(urlad(this.pageNumber+1, this.description)) // загружаем данные
             .then(response => (this.nextResults = response.data)); // получаем  ответ
             
         },
         fetchData: function () {
             axios
-            .get(urlad(this.pageNumber)) // загружаем данные
+            .get(urlad(this.pageNumber, this.description)) // загружаем данные
             .then(response => (this.results = response.data)) // получаем  ответ
             .catch(error => {
                 console.log(error);
@@ -56,24 +56,39 @@
             .finally(() => (this.loading = false));
         },
         nextPage: function(){
-            this.pageNumber++;
-            this.loading = true;
-            this.fetchData();
-            this.nextData();
-  
+          this.pageNumber++;
+          this.fetchData();
+          this.nextData(); 
          },
-         prevPage: function(){
+        prevPage: function(){
           this.pageNumber--;
-          this.loading = true;
           this.fetchData(); 
           this.nextData();         
-         },
-
+        },
+        search: function(){
+          let loc  = "";
+          let desc  = "";
+          let check  = "";
+          if(this.$refs.local.value) loc = '&location='+ this.$refs.local.value;
+          if(this.$refs.descr.value) desc = '&description='+ this.$refs.descr.value;  
+          check = '&full_time='+ this.checked;       
+          this.description = loc + desc + check;
+          this.fetchData();
+          this.nextData();
+        },
+        cleanAll: function(){
+          Object.assign(this.$data, this.$options.data.call(this));
+          this.fetchData();
+          this.nextData();
+        },
+        transferto: function() {
+          this.$emit("transfer", this.$refs.vacancy.value);
+        }
       },
     filters: {
         setDate (value) {
           return new Date(value).toLocaleDateString();
-        }
+        },
     },
     computed: {
       getNextURLEmpty() {  //проверка на пустоту данных получаемых с json
@@ -83,10 +98,24 @@
           }          
         }
         return false;
+      },
+      emptyData() {
+        console.log(this.results);
+        if(this.results){
+          if(this.results.length != 0) {
+            return false;
+          }          
+        }
+        return true;
       }
     }
     
   });
 
-
+var router = new VueRouter({
+    routes: [
+      { path: 'vacancy/:name', component: preview }
+    ]
+});
+  
    
